f3ac47c0c6f39a3b0d3549a2dacf932d24e3b46a
ola2207 <89849676+ola2207@users.noreply.github.com>
2022-04-28 15:52:49 +0200

    fisk

---
+++ src/main/java/com/example/prototypevisualisering/PrototypeVisualiseringApplication.java
@@ -0,0 +1,13 @@
+package com.example.prototypevisualisering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeVisualiseringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeVisualiseringApplication.class, args);
+    }
+
+}
